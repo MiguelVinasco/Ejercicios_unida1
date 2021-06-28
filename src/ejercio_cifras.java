72d2b42588925d4a7453b395074abac1e5cd0bc6
miguel_vinasco <miguelvinasco99@gmail.com>
2021-06-28 10:40:59 -0500

    inicio del proyecto

---
+++ src/ejercio_cifras.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class ejercio_cifras {
+
+    public static void main(String[] args) {
+        
+        Scanner entrada = new Scanner(System.in);
+
+
+        System.out.println("Ingresa el numero:");
+
+        var numero = entrada.nextInt();
+
+        var digito = cifras(numero);
+
+        System.out.println("el numero tiene "+ digito + " cifras");
+
+
+    }
+
+    public static int cifras(int numero){
+
+        var cifras = 0;
+
+        while (numero != 0) {
+
+            numero /= 10;
+            cifras++;          
+        }
+
+        return cifras;
+
+
+
+    }
+    
+}
+
